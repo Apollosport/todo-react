c5b36d807ef94187a4cb576e745769a3c3ed8b5e
Stefaan-Luucian <lucianbutnaru@gmail.com>
2022-11-10 20:17:52 +0100

    my first commit

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -2,6 +2,11 @@ import React from "react";
 import { BsTrashFill } from "react-icons/bs";
 import { BsTrash } from "react-icons/bs";
 
+const Handleediting = () => {
+  
+
+}
+
 const Todo = ({ todo, list, setList }) => {
   const deleteHandler = () => {
     setList(list.filter((element) => element.id !== todo.id));
@@ -25,10 +30,13 @@ const Todo = ({ todo, list, setList }) => {
     <div className="todo">
       <input type="checkbox" onChange={doneHandler}></input>
       <li className={`todo-item ${todo.done ? "done" : ""}`}>{todo.text}</li>
-
       <button className="delete" onClick={deleteHandler}>
         <BsTrashFill /> <BsTrash />
       </button>
+      <button className="edit" onClick={deleteHandler}>
+        Edit
+      </button>
+      
     </div>
   );
 };
